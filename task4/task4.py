0b0d3011d3c53f4914a50d965e55842a3e223c5c
aynursune <aynur.suene@tum.de>
2020-06-02 03:11:37 +0200

    Task 4 Logistic Map implemented

--- task4/task4.py
+++ task4/task4.py
@@ -26,7 +26,7 @@ def plotLorenz(x0_x,ro):
 
 	solution = solve_ivp(lorenzEquations, time, x0, args=(sigma,ro,beta))
 	sol = solution.y
-	print(sol)
+
 	fig = plt.figure()
 	ax0 = fig.gca(projection = '3d')
 	ax0.plot(sol[0,:], sol[1,:], sol[2,:], linewidth = width, color= c, linestyle=':', antialiased=True)
@@ -125,7 +125,83 @@ def computeDifference(s1,s2):
 	return distArr, distArrIndex
 
 
+def logisticEquation(x,r):
+	return r * x * (1 - x)
+
+def plotLogistic(r_min,r_max):
+	'''	
+		https://mathworld.wolfram.com/LogisticMap.html:
+
+		 1: 1
+
+		 These solutions are only real for r>=3, so this is where the 2-cycle begins. so r_2=3 is the onset of period doubling
+		 first bifurcation occurs at around r=3.
+
+		 3-cycle starts at r_3 = 3.828427
+
+		 The 4-cycle therefore starts at 3.449489... 
+
+		 5-cycles r_5=3.73817... (OEIS A118452), 3.90557..., and 3.99026....
+		 
+		 6: 3.62655316..
+
+		 7: 3.70164076...
+		
+		 8-cycles: 3.54409
+
+		 16: 3.56440726...
+
+		http://www.maths.qmul.ac.uk/~sb/cf_chapter3.pdf:
+
+		 The birth of the period 2 attractor is an example of a period-doubling (or pitchfork) bifurcation. 
+		 The ‘handle’ of the pitchfork is the (μ, x) graph of the attracting fixed point x = 1 − 1/μ and this curve continues as the ‘middle prong’ when the fixed point becomes a repeller. 
+		 The other two prongs are made up of the graph of the period 2 attracting cycle x2 − ((μ + 1)/μ)x + (μ + 1)/μ2 = 0.
+
+		By contrast the period 3 attractor is born via a tangent (or saddle) bifurcation. 
+		Here, at the value μ = 3.829, the graph of fμ3 touches the line y = x tangentially at three points (it still crosses the line y = x at the point x = 1 − 1/μ). 
+		As μ passes this value what happens is that a pair of complex conjugate period 3 orbits collide and become a real period 3 orbit, which then separates into a pair of period 3 orbits, one attracting and one repelling. 
+		For values of μ just below 3.829 the system behaves for long periods as if it had a period 3 attractor, but has bursts of chaotic behaviour in between these long periods. This phenomenon is called intermittency.
+	'''
+	
+	R = np.arange(r_min+0.001, r_max+0.001,0.001)
+	values = []
+	r_val = []
+	for r in R:
+	    x = np.random.random()
+	    for n in range(1000):   # we are saving the solution after 1000 iteration
+	    	x = logisticEquation(x,r)
+	    	if n > 939:  # saving last 60
+	    		values.append(x)
+	    		r_val.append(r)
+	values = np.array(values)
+	r_val = np.array(r_val)
+
+	fig = plt.figure(figsize=(12,7))
+	ax0 = fig.add_subplot()
+	ax0.scatter(r_val, values, s = 0.1, color= 'seagreen')
+
+	if r_min == 0.0:
+		plt.axvline(x=1.0, linewidth=0.5, label="Fixed Point", c="lightpink" )
+	if r_max == 4.0:
+		plt.axvline(x=3.0, linewidth=0.5, label="2-cycle, Pitchfork Bifurcation", c="darkorange")  # 2-cycle = period doubling. first bifurcation (pitchfork)
+		plt.axvline(x=3.828427, linewidth=0.5, label="3-cycle, Saddle Bifurcation", c="indianred" )  # 3-cycle, saddle bifurcation
+		plt.axvline(x=3.449489, linewidth=0.5, label="4-cycle", c="deepskyblue")  # 4-cycle
+		plt.axvline(x=3.73817, linewidth=0.5, label="5-cycle", c="slateblue")  # 5-cycle
+		plt.axvline(x=3.62655316, linewidth=0.5, label="6-cycle",c="maroon")  # 6-cycle
+		plt.axvline(x=3.70164076, linewidth=0.5, label="7-cycle", c="mediumvioletred")  # 7-cycle
+		plt.axvline(x=3.54409, linewidth=0.5, label="8-cycle", c="slategrey")  # 8-cycle
+		plt.axvline(x=3.56440726, linewidth=0.5, label="16-cycle", c="goldenrod")  # 16-cycle
+	
+	plt.legend(loc='upper left',prop={'size': 8})
+	plt.xlabel("r")
+	plt.ylabel("x")
+	plt.title(("Bifurcation Diagram of Logistic Map, r = ({},{}]").format(int(r_min),int(r_max)))
+	plt.show()
+	
+
 def main():
+	# Lorenz Attractor
+
 	x0_x = 10
 	ro = 0.5
 	s1 = plotLorenz(x0_x,ro)
@@ -134,12 +210,16 @@ def main():
 	#print("s1 time: ", s2.t[-2])
 	
 	differentPointArray, indexes = computeDifference(s1,s2)
-
-	print(len(differentPointArray))
 	plotLorenzWithPoints(s1.y,s2.y,indexes)
 
 
+	# Logistic Map
+
+	plotLogistic(0.0,4.0) # (0,4]
+	plotLogistic(0.0,2.0)  # (0,2]
+	plotLogistic(2.0,4.0)  # (2,4]
 
 
 if __name__ == '__main__':
 	main()
+
